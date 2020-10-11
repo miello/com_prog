57957172dc0eee4d2c60396c648039a1edf57972
miello <partearth1212@gmail.com>
2020-10-11 20:19:41 +0700

    🐛 Fix Wrong Code

--- P3/P3_09_BNK48.py
+++ P3/P3_09_BNK48.py
@@ -22,36 +22,31 @@ while True:
 
     if not vote_list[person].get(vote_to):
         vote_list[person][vote_to] = 0
+        kami_ochi[person] = ['', 0]
 
+    cnt_kami[vote_to] = 0
     vote_list[person][vote_to] += vote_score
 
 for x in list(vote_list.items()):
+    person_name = x[0]
     for a in x[1]:
         name = a
         value = x[1][a]
+
         if not score.get(name):
             score[name] = [0, 0]
+
         score[name][0] += value
         score[name][1] += 1
 
-        if not kami_ochi.get(x[0]):
-            kami_ochi[x[0]] = ['', -1]
-
-        if kami_ochi[x[0]][1] < value:
-            kami_ochi[x[0]][1] = value
-            kami_ochi[x[0]][0] = name
-        elif kami_ochi[x[0]][1] == value:
-            if kami_ochi[x[0]][0] > name:
-                kami_ochi[x[0]][0] = name
+        if kami_ochi[person_name][1] < value:
+            kami_ochi[person_name][1] = value
+            kami_ochi[person_name][0] = name
+        elif kami_ochi[person_name][1] == value and kami_ochi[person_name][0] > name:
+            kami_ochi[person_name][0] = name
 
 for name, value in list(kami_ochi.values()):
-    if not cnt_kami.get(name):
-        cnt_kami[name] = 0
     cnt_kami[name] += 1
-
-print(kami_ochi)
-
-print(cnt_kami)
 
 if query == 1:
     ans = [e[0] for e in sorted(score.items(), key=lambda x: -x[1][0])][0:3]
